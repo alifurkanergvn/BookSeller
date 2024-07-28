c27650abc662726f9182a013cf5259b4ff82b9db
Ali Furkan ERGÜVEN <95187782+alifurkanergvn@users.noreply.github.com>
2024-07-28 11:17:47 +0300

    Update IBookRepository.java

--- src/main/java/com/afe/bookseller/repository/IBookRepository.java
+++ src/main/java/com/afe/bookseller/repository/IBookRepository.java
@@ -3,10 +3,6 @@ package com.afe.bookseller.repository;
 import com.afe.bookseller.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * @author sa
- * @date 3.07.2021
- * @time 17:55
- */
+
 public interface IBookRepository extends JpaRepository<Book, Long> {
 }
