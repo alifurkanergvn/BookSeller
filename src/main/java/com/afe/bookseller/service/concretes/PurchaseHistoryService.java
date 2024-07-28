8b9c2817065ddac3f8ae7ab19341df522c1c3f68
Ali Furkan ERGÜVEN <95187782+alifurkanergvn@users.noreply.github.com>
2024-07-28 11:12:05 +0300

    Update PurchaseHistoryService.java

--- src/main/java/com/afe/bookseller/service/concretes/PurchaseHistoryService.java
+++ src/main/java/com/afe/bookseller/service/concretes/PurchaseHistoryService.java
@@ -9,11 +9,7 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- * @author sa
- * @date 3.07.2021
- * @time 18:14
- */
+
 @Service
 public class PurchaseHistoryService implements IPurchaseHistoryService
 {
